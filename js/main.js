46019490421787799d54b5c4b817ebe7a36306c2
spicyChick3ns <gonzalesmsd@gmail.com>
2017-07-20 11:20:48 -0400

    updated JS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-var slushBtnPress= ["#sBtn", "#mBtn", "#slushBtn" ]
 $(document).ready(function(){
     $("ul").hide();
+    $("li").css("background-color", "#F5F5F5");
 
-    // $("#sBtn").click(function()) {
-    //     $("#mBtn").hide();
-    // });
+
     $("#sBtn").click(function(){
        $("#mBtn").fadeOut();
        $("#slushBtn").fadeOut();
        $("#scoopFlavour").slideDown();
-
+      });
+    $("#mBtn").click(function(){
+       $("#sBtn").fadeOut();
+       $("#slushBtn").fadeOut();
+       $("#milkFlavour").slideDown();
+      });
+    $("#slushBtn").click(function(){
+       $("#mBtn").fadeOut();
+       $("#sBtn").fadeOut();
+       $("#slushFlavour").slideDown();
       });
 });
